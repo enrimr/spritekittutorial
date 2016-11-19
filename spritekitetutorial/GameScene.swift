739ce7c4e76b7576aeab4cc6b27b248f9a44982c
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2016-11-19 14:34:06 +0100

    Add touchesBegan to get touch position and spaceship follow cursor direction

--- spritekitetutorial/GameScene.swift
+++ spritekitetutorial/GameScene.swift
@@ -13,7 +13,8 @@ class GameScene: SKScene {
     
     let redRectangule = SKSpriteNode(color: UIColor.red, size: CGSize(width: 200, height: 140));
     let spaceshipImage = SKSpriteNode(imageNamed: "Spaceship");
-    
+    var xDirection = CGFloat(1);
+    var yDirection = CGFloat(1);
     override func didMove(to view: SKView) {
 
         redRectangule.position = CGPoint(x: 0, y: 0);
@@ -26,10 +27,27 @@ class GameScene: SKScene {
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        spaceshipImage.position.y += 1;
+
         spaceshipImage.zRotation += 0.1;
+        
+        for touch in touches {
+            let location = touch.location(in: self);
+            
+            if location.x > spaceshipImage.position.x {
+                xDirection = CGFloat(1);
+            } else {
+                xDirection = CGFloat(-1);
+            }
+            
+            if location.y > spaceshipImage.position.y {
+                yDirection = CGFloat(1);
+            } else {
+                yDirection = CGFloat(-1);
+            }
+        }
     }
     override func update(_ currentTime: TimeInterval) {
-
+        spaceshipImage.position.x += xDirection;
+        spaceshipImage.position.y += yDirection;
     }
 }

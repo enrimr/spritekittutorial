fd69f348d8bf016a094697c863331b70fad1a1cd
Enrique Ismael Mendoza Robaina <enriquemendozarobaina@gmail.com>
2016-11-19 13:20:17 +0100

    Add Spaceship image to scene

--- spritekitetutorial/GameScene.swift
+++ spritekitetutorial/GameScene.swift
@@ -13,7 +13,10 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         let myNode = SKSpriteNode(color: UIColor.red, size: CGSize(width: 200, height: 140));
         myNode.position = CGPoint(x: 0, y: 0);
-        
         self.addChild(myNode);
+        
+        let myImage = SKSpriteNode(imageNamed: "Spaceship");
+        myImage.position = CGPoint(x: 0, y: 0);
+        self.addChild(myImage);
     }
 }
